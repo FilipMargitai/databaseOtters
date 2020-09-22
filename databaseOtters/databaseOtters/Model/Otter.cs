90508b2e96b917244887e3ce699cb03fbbfdf792
Filip Margitai <u18.415@pslib.cloud>
2020-09-22 14:36:20 +0200

    classes done

---
+++ databaseOtters/databaseOtters/Model/Otter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace databaseOtters.Model
+{
+    public class Otter
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        [Key]
+        public int tattooID { get; set; } 
+    }
+}
