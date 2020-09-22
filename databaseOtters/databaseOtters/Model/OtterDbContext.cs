bb1feead2296d8e9c77c41d367af31391976dfd5
Filip Margitai <u18.415@pslib.cloud>
2020-09-22 14:29:39 +0200

    dbcontext

---
+++ databaseOtters/databaseOtters/Model/OtterDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace databaseOtters.Model
+{
+    public class OtterDbContext : DbContext
+    {
+        public OtterDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+    }
+    public virtual DbSet<Otter> Otters { get; set; }
+}
