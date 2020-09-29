bc97d51449a3fd56e029c1624ba5d415d1cdd78a
Filip Margitai <u18.415@pslib.cloud>
2020-09-29 15:12:25 +0200

    changes

--- databaseOtters/databaseOtters/Model/OtterDbContext.cs
+++ databaseOtters/databaseOtters/Model/OtterDbContext.cs
@@ -26,25 +26,25 @@ namespace databaseOtters.Model
 
             modelBuilder.Entity<Place>().HasKey(p => new { p.Name, p.LocationId }); // složené id (z jména a lokace)
 
-            var plc1 = new Place { Name = "U Studánky", location = loc1 };
-            var plc2 = new Place { Name = "U Buku", location = loc1 };
-            var plc3 = new Place { Name = "Černé jezero", location = loc1 };
+            var plc1 = new Place { Name = "U Studánky", LocationId = 111 };
+            var plc2 = new Place { Name = "U Buku", LocationId = 128 };
+            var plc3 = new Place { Name = "Černé jezero", LocationId = 128 };
 
             modelBuilder.Entity<Place>().HasData(
                 plc1, 
                 plc2, 
-                new Place { Name = "U Studánky", location = loc3},
+                new Place { Name = "U Studánky", LocationId = 76},
                 plc3,
-                new Place { Name = "Slovenský pařez", location = loc2},
-                new Place { Name = "Šalina", location = loc3}
+                new Place { Name = "Slovenský pařez", LocationId = 128},
+                new Place { Name = "Šalina", LocationId = 76}
                 );
 
-            var pramatka = new Otter { Name = "Matějova máma", tattooID = 1, Color = "žlutá", place = plc1 };
+            var pramatka = new Otter { Name = "Matějova máma", tattooID = 1, Color = "žlutá", PlaceName = "U Studánky" };
 
             modelBuilder.Entity<Otter>().HasData(
                 pramatka,
-                new Otter { Name = "Magnet", tattooID = 11, Color = "negr", place = plc3, Mother = pramatka},
-                new Otter { Name = "Macháček", tattooID = 21, Color = "lososová", place = plc1, Mother = pramatka }
+                new Otter { Name = "Magnet", tattooID = 11, Color = "negr", PlaceName = "Černé jezero", Mother = pramatka},
+                new Otter { Name = "Macháček", tattooID = 21, Color = "lososová", PlaceName = "U Studánky", Mother = pramatka }
                 );
         }
     }
